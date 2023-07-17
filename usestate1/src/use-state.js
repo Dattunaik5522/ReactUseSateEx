8aab0306d44f4bba5477b6bc768e2535576cdd72
Dattunaik5252 <gugulothdattu000@gmail.com>
2023-07-17 11:52:10 +0530

    Handled edit button

--- usestate1/src/use-state.js
+++ usestate1/src/use-state.js
@@ -28,7 +28,7 @@ const Usestateex = () => {
       text: message.text,
       id: new Date().getTime().toString(),
     };
-    // console.log(newTodo);
+    console.log("previous Todos", newTodo);
     setList([...list, newTodo]);
     setMessage({
       text: "",
@@ -36,12 +36,56 @@ const Usestateex = () => {
     });
   };
 
+  const [editItem, setEditItem] = useState({
+    id: "",
+    isEditing: false,
+  });
+
   const handleDelete = (id) => {
     console.log(id);
     let newTodos = list.filter((eachitem) => {
       return eachitem.id != id;
     });
     setList(newTodos);
+  };
+
+  const handleEdit = (id) => {
+    // console.log(id);
+    setEditItem({
+      ...editItem,
+      id: id,
+      isEditing: true,
+    });
+    let editableItem = list.find((eachItem) => eachItem.id === id);
+    console.log(editableItem);
+    setMessage({
+      text: editableItem.text,
+      id: editItem.id,
+    });
+  };
+
+  const handleEditInForm = (e) => {
+    e.preventDefault();
+    let newTodos = list.map((eachItem) => {
+      if (eachItem.id === editItem.id) {
+        return {
+          text: message.text,
+          id: editItem.id,
+        };
+      } else {
+        return eachItem;
+      }
+    });
+    setList(newTodos);
+    setMessage({
+      text: "",
+      id: "",
+    });
+    setEditItem({
+      isEditing: false,
+      id: "",
+    });
+    console.log("new Todo's:", newTodos);
   };
 
   return (
@@ -55,18 +99,25 @@ const Usestateex = () => {
           value={message.text}
           onChange={changeMessage}
         />
-        <button onClick={handleSubmit} type="submit">
-          add
-        </button>
+        {editItem.isEditing ? (
+          <button onClick={handleEditInForm} type="submit">
+            edit
+          </button>
+        ) : (
+          <button onClick={handleSubmit} type="submit">
+            add
+          </button>
+        )}
       </form>
       <hr />
+      {list.length === 0 && <h1>There is no items in the Todos</h1>}
       <ul>
         {list.map((eachitem) => {
-          console.log(eachitem);
           const { text, id } = eachitem;
           return (
             <li key={id}>
-              <div>{text}</div> <button>edit</button>
+              <div>{text}</div>{" "}
+              <button onClick={() => handleEdit(id)}>edit</button>
               <button onClick={() => handleDelete(id)}>delete</button>
             </li>
           );
